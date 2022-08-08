3f539575bfc44f2b9f3dd5bea79c1fe00d983e12
anirudhlakhotia <sanjeev196945@gmail.com>
2022-08-08 16:20:35 +0530

    :construction: Added a few features
    Work in progress

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,16 +2,18 @@ import "./App.css";
 import Landing from "./components/Landing";
 import Dashboard from "./components/Dashboard";
 import { BrowserRouter,Routes, Route } from "react-router-dom";
-// import Navbar  from "./utils/Navbar";
+import Test from "./components/Test";
+import Navbar  from "./utils/Navbar";
 
 function App() {
   return (
     <>
       <BrowserRouter>
-        {/* <Navbar/> */}
+        <Navbar/>
         <Routes>
           <Route exact path="/" element={<Landing />} />
           <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/test" element={<Test />} />
         </Routes>
       </BrowserRouter>
     </>
