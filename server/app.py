9206e698e58b0a0c6a0d4f86c0fc20a9673daae8
B Monish Moger <85382114+senju-hashirama@users.noreply.github.com>
2022-08-08 09:14:49 +0530

    Update app.py

--- server/app.py
+++ server/app.py
@@ -7,6 +7,11 @@ import subprocess
 from werkzeug.utils import secure_filename
 import os
 import requests
+import base64
+from cryptography.fernet import Fernet
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+import bcrypt
 app=Flask(__name__)
 
 def encrypting(password, pepper):
